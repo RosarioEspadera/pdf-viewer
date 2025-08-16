fd88603cbf3a335e210ca6c81d59032eb6656eb3
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-16 07:38:18 +0000

    a16

--- plugins/gestures.js
+++ plugins/gestures.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("pdf-canvas");
+  if (canvas) {
+    // your gesture setup here
 let startX = 0;
 canvas.addEventListener('touchstart', e => {
   startX = e.touches[0].clientX;
@@ -7,4 +11,8 @@ canvas.addEventListener('touchend', e => {
   let endX = e.changedTouches[0].clientX;
   if (endX - startX > 50) document.getElementById('prevPage').click();
   else if (startX - endX > 50) document.getElementById('nextPage').click();
+});
+} else {
+    console.warn("Canvas element not found");
+  }
 });
